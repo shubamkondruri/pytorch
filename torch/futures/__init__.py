f50e672121b8f6670c7ec42856ff480efef829d9
Shihao Xu <xsh6528@gmail.com>
2020-06-22 22:14:35 -0700

    [JIT x RPC] Consolidate Future type class and Future impl class
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/40406
    
    Same motivation for https://github.com/pytorch/pytorch/issues/35110.
    
    `Future` and `RRef` are two important types for `rpc` module, should make users feel easy to use.
    
    Follow https://github.com/pytorch/pytorch/pull/35694.
    ghstack-source-id: 106402439
    
    Differential Revision: [D7722176](https://our.internmc.facebook.com/intern/diff/D7722176/)

--- torch/futures/__init__.py
+++ torch/futures/__init__.py
@@ -1,14 +1,18 @@
+from typing import Generic, TypeVar
+
 import torch
 
 
-class Future(torch._C.Future):
+T = TypeVar("T")
+GenericWithOneTypeVar = Generic[T]
+
+
+class PyFuture(torch._C.Future):
     r"""
     Wrapper around a ``torch._C.Future`` which encapsulates an asynchronous
     execution of a callable, e.g. :meth:`~torch.distributed.rpc.rpc_async`. It
     also exposes a set of APIs to add callback functions and set results.
     """
-    def __new__(cls):
-        return super(Future, cls).__new__(cls)
 
     def wait(self):
         r"""
@@ -19,7 +23,7 @@ class Future(torch._C.Future):
             creating the value has thrown an error, this ``wait`` method will
             also throw an error.
         """
-        return super(Future, self).wait()
+        return super().wait()
 
     def then(self, callback):
         r"""
@@ -58,7 +62,7 @@ class Future(torch._C.Future):
             >>> # RPC return value is 5.
             >>> # Chained cb done. None
         """
-        return super(Future, self).then(callback)
+        return super().then(callback)
 
     def set_result(self, result):
         r"""
@@ -88,7 +92,7 @@ class Future(torch._C.Future):
             >>> print(fut.wait())  # tensor([3., 3.])
             >>> t.join()
         """
-        super(Future, self).set_result(result)
+        super().set_result(result)
 
 
 def collect_all(futures):
@@ -124,6 +128,7 @@ def collect_all(futures):
     """
     return torch._C._collect_all(futures)
 
+
 def wait_all(futures):
     r"""
     Waits for all provided futures to be complete, and returns
@@ -138,3 +143,21 @@ def wait_all(futures):
         :class:`~torch.futures.Future` throws.
     """
     return [fut.wait() for fut in torch._C._collect_all(futures).wait()]
+
+
+try:
+
+    class Future(PyFuture, GenericWithOneTypeVar):
+        # Combine the implementation class and the type class.
+        pass
+
+
+except TypeError as exc:
+    # TypeError: metaclass conflict: the metaclass of a derived class
+    # must be a (non-strict) subclass of the metaclasses of all its bases
+    class FutureMeta(PyFuture.__class__, GenericWithOneTypeVar.__class__):
+        pass
+
+    class Future(PyFuture, GenericWithOneTypeVar, metaclass=FutureMeta):
+        # Combine the implementation class and the type class.
+        pass
